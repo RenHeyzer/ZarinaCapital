24fc409139fedd9a931be2f499084663f60c3630
RenHeyzer <vesolyyradin@gmail.com>
2024-03-11 13:25:21 +0600

    Подключены все модули

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,17 @@ dependencyResolutionManagement {
 
 rootProject.name = "ZarinaCapital"
 include(":app")
- 
+include(":core:data")
+include(":core:common")
+include(":core:presentation")
+include(":features:sign-up")
+include(":features:my-courses")
+include(":features:courses")
+include(":features:menu")
+include(":data:courses")
+include(":features:sign-in")
+include(":data:menu")
+include(":data:my-courses")
+include(":data:sign-in")
+include(":data:sign-up")
+include(":core:theme")
