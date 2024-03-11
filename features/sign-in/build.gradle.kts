dc6824acdc38667b9d7068d1921df33b78cfb3a5
RenHeyzer <vesolyyradin@gmail.com>
2024-03-11 14:17:21 +0600

    Фича для экрана "Вход"

---
+++ features/sign-in/build.gradle.kts
@@ -0,0 +1,49 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.ren.signin"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.fragment.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.view.binding.property.delegate.noreflection)
+
+    implementation(project(":core:theme"))
+    implementation(project(":core:presentation"))
+}
---
+++ features/sign-in/src/main/java/com/ren/signin/internal/domain/Stub.kt
@@ -0,0 +1,2 @@
+package com.ren.signin.internal.domain
+
