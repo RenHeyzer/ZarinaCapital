a52861b4ebd215f72ca6d6c649445709ef3bc520
RenHeyzer <vesolyyradin@gmail.com>
2024-03-23 01:29:05 +0600

    Изменено с Factory на Builder. Предоставление зависимостей для фичи Auth. Получение base url через Builder

--- app/src/main/java/com/ren/zarinacapital/di/AppComponent.kt
+++ app/src/main/java/com/ren/zarinacapital/di/AppComponent.kt
@@ -1,17 +1,41 @@
 package com.ren.zarinacapital.di
 
 import android.content.Context
+import com.geeks.di.dependencies.HasFeatureDependencies
+import com.ren.presentation.utils.ExceptionMessages
 import dagger.BindsInstance
 import dagger.Component
+import dagger.Module
+import dagger.Provides
 import javax.inject.Singleton
 
-@Singleton
-@Component(modules = [AppSubcomponents::class])
-interface AppComponent {
+@[Singleton Component(
+    modules = [
+        AppModule::class,
+        FeatureDepsModule::class,
+        ApiModule::class
+    ],
+)]
+interface AppComponent : HasFeatureDependencies {
 
-    @Component.Factory
-    interface Factory {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun applicationContext(context: Context): Builder
 
-        fun create(@BindsInstance context: Context): AppComponent
+        @BindsInstance
+        fun baseUrl(@ForexBaseUrl baseUrl: String): Builder
+
+        fun build(): AppComponent
     }
+
+//    fun authAdapterComponent(): AuthAdapterComponent.Factory
+}
+
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideExceptionMessages(context: Context) = ExceptionMessages(context)
 }
