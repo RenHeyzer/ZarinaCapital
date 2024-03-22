c7e13bafd213fbf46d96a5f8d5aca766f063d1e4
RenHeyzer <vesolyyradin@gmail.com>
2024-03-23 01:23:58 +0600

    Переименован

---
+++ app/src/main/java/com/ren/zarinacapital/ZarinaApp.kt
@@ -0,0 +1,25 @@
+package com.ren.zarinacapital
+
+import android.app.Application
+import com.geeks.di.dependencies.FeatureDependenciesProvider
+import com.geeks.di.dependencies.HasFeatureDependencies
+import com.ren.zarinacapital.di.AppComponent
+import com.ren.zarinacapital.di.DaggerAppComponent
+
+class ZarinaApp : Application(), HasFeatureDependencies {
+
+    override lateinit var featureDependencies: FeatureDependenciesProvider
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .applicationContext(applicationContext)
+            .baseUrl(BuildConfig.FOREX_BASE_URL)
+            .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+//        appComponent.authAdapterComponent().create()
+        featureDependencies = appComponent.featureDependencies
+    }
+}
