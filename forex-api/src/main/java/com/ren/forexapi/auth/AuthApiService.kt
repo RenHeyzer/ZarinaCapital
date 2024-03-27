0c783a435bb658e2713e63011f4315c284f43755
RenHeyzer <vesolyyradin@gmail.com>
2024-03-28 01:51:23 +0600

    interface для описания запросов связанных с аутентификацией

---
+++ forex-api/src/main/java/com/ren/forexapi/auth/AuthApiService.kt
@@ -0,0 +1,22 @@
+package com.ren.forexapi.auth
+
+import com.ren.forexapi.models.UserDTO
+import com.ren.forexapi.models.VerificationCodeDTO
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+private const val REGISTER_END_POINT = "register/"
+private const val CONFIRM_END_POINT = "confirm/"
+
+interface AuthApiService {
+
+    @POST(REGISTER_END_POINT)
+    suspend fun signUp(
+        @Body user: UserDTO
+    ): UserDTO
+
+    @POST(CONFIRM_END_POINT)
+    suspend fun confirmEmail(
+        @Body verificationCodeDTO: VerificationCodeDTO
+    )
+}
