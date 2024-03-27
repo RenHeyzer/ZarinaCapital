4bf4451f6ac0fdecd594622a8bdd2347ff49eefe
RenHeyzer <vesolyyradin@gmail.com>
2024-03-28 01:11:18 +0600

    Подключены модули core, domain:auth и forex-api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,14 +57,22 @@ dependencies {
 
     implementation(libs.bundles.navigation)
 
-    // Theme
+    // Core
     implementation(project(":core:theme"))
+    implementation(project(":core:presentation"))
+    implementation(project(":core:di"))
+
+    // Api
+    implementation(project(":forex-api"))
 
     // Data
     implementation(project(":data:courses"))
     implementation(project(":data:menu"))
     implementation(project(":data:my-courses"))
     implementation(project(":data:auth"))
+
+    // Domain
+    implementation(project(":domain:auth"))
 
     // Features
     implementation(project(":features:courses"))
