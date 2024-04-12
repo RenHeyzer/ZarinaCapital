3e0787166917efa266a2366eb254251609d4e592
RenHeyzer <vesolyyradin@gmail.com>
2024-04-13 02:59:56 +0600

    Удалил модули data:auth, data:my-courses, data:menu

--- domain/auth/src/main/java/com/ren/auth/exceptions/EmptyFieldsException.kt
+++
@@ -1,8 +0,0 @@
-package com.ren.auth.exceptions
-
-import com.ren.auth.entities.SignUpField
-
-class EmptyFieldsException(
-    val emptyFields: Map<SignUpField, Boolean>,
-    val exceptionMessages: Map<SignUpField, String?>,
-) : Exception()
