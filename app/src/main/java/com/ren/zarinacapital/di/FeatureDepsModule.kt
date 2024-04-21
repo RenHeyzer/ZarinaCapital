97d555988f1e561e8541775ef95e245caa52095f
RenHeyzer <vesolyyradin@gmail.com>
2024-04-22 03:19:30 +0600

    additionally(app): provide "CoursesDepsComponent" as "CoursesComponent" dependencies

--- app/src/main/java/com/ren/zarinacapital/di/FeatureDepsModule.kt
+++ app/src/main/java/com/ren/zarinacapital/di/FeatureDepsModule.kt
@@ -3,10 +3,13 @@ package com.ren.zarinacapital.di
 import android.content.Context
 import com.ren.auth.api.dependencies.AuthDeps
 import com.ren.common.AppDispatchers
+import com.ren.courses.api.dependencies.CoursesDeps
 import com.ren.di.dependencies.FeatureDependencies
 import com.ren.di.keys.FeatureDependenciesKey
 import com.ren.forexapi.auth.AuthApiService
+import com.ren.forexapi.courses.CoursesApiService
 import com.ren.zarinacapital.auth.di.DaggerAuthDepsComponent
+import com.ren.zarinacapital.courses.di.DaggerCoursesDepsComponent
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
@@ -26,6 +29,19 @@ object FeatureDepsModule {
             .context(context)
             .appDispatchers(appDispatchers)
             .registerApiService(authApiService)
+            .build()
+    }
+
+    @Provides
+    @IntoMap
+    @FeatureDependenciesKey(CoursesDeps::class)
+    fun provideCoursesDeps(
+        appDispatchers: AppDispatchers,
+        coursesApiService: CoursesApiService
+    ): FeatureDependencies {
+        return DaggerCoursesDepsComponent.builder()
+            .appDispatchers(appDispatchers)
+            .coursesApiService(coursesApiService)
             .build()
     }
 }
