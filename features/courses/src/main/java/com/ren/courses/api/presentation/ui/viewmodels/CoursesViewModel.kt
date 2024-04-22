ea136aeca60d302cc909d752d4210b2d845304c5
RenHeyzer <vesolyyradin@gmail.com>
2024-04-22 17:07:27 +0600

    additionally(features:courses): get courses from repository and collect to "LiveData" with state

--- features/courses/src/main/java/com/ren/courses/api/presentation/ui/viewmodels/CoursesViewModel.kt
+++ features/courses/src/main/java/com/ren/courses/api/presentation/ui/viewmodels/CoursesViewModel.kt
@@ -1,6 +1,21 @@
 package com.ren.courses.api.presentation.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ren.courses.entities.Course
+import com.ren.courses.repositories.CoursesRepository
 import com.ren.presentation.base.BaseViewModel
+import com.ren.presentation.utils.UIState
+import javax.inject.Inject
 
-class CoursesViewModel : BaseViewModel() {
+class CoursesViewModel @Inject constructor(
+    private val coursesRepository: CoursesRepository
+) : BaseViewModel() {
+
+    private val _coursesState = MutableLiveData<UIState<List<Course>>>(UIState.Loading)
+    val coursesState: LiveData<UIState<List<Course>>> = _coursesState
+
+    init {
+        coursesRepository.fetchCourses().collectFlowAsState(_coursesState)
+    }
 }
