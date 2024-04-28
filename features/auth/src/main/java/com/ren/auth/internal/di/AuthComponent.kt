4d6843a60850ecbce894af6cae06c1cc8ae91418
RenHeyzer <vesolyyradin@gmail.com>
2024-04-28 17:50:29 +0600

    additionally(features:auth): binds from "SignUpComponent.kt" and provision property "viewModelFactory"
    * binds "SignUpParamsMapper.kt", base class "ViewModelFactory.kt", "SignUpViewModel.kt"

--- features/auth/src/main/java/com/ren/auth/internal/di/AuthComponent.kt
+++ features/auth/src/main/java/com/ren/auth/internal/di/AuthComponent.kt
@@ -1,16 +1,24 @@
 package com.ren.auth.internal.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.ren.auth.api.dependencies.AuthDeps
 import com.ren.auth.api.domain.entities.User
 import com.ren.auth.api.domain.repositories.AuthRepository
 import com.ren.auth.internal.data.mappers.UserMapper
 import com.ren.auth.internal.data.repositories.AuthDataRepository
+import com.ren.auth.internal.domain.entities.SignUpParams
+import com.ren.auth.internal.domain.mappers.SignUpParamsMapper
+import com.ren.auth.internal.presentation.ui.viewmodels.SignUpViewModel
 import com.ren.common.Mapper
+import com.ren.di.keys.ViewModelKey
 import com.ren.di.scopes.FeatureScope
 import com.ren.forexapi.api.models.UserDTO
+import com.ren.presentation.base.ViewModelFactory
 import dagger.Binds
 import dagger.Component
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @[FeatureScope Component(
     modules = [AuthModule::class],
@@ -18,7 +26,7 @@ import dagger.Module
 )]
 internal interface AuthComponent {
 
-    fun signUpComponent(): SignUpComponent.Factory
+    val viewModelFactory: ViewModelProvider.Factory
 }
 
 @Module
@@ -28,5 +36,16 @@ internal interface AuthModule {
     fun bindUserMapper(mapperImpl: UserMapper): Mapper<UserDTO, User>
 
     @Binds
+    fun bindSignUpParamsMapper(mapperImpl: SignUpParamsMapper): Mapper<SignUpParams, User>
+
+    @Binds
     fun bindAuthRepository(repositoryImpl: AuthDataRepository): AuthRepository
+
+    @Binds
+    fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SignUpViewModel::class)
+    fun signUpViewModel(viewModel: SignUpViewModel): ViewModel
 }
