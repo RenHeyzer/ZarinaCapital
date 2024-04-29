b208543913496aa0f012f3465ee50192fa16f4a9
RenHeyzer <vesolyyradin@gmail.com>
2024-04-29 22:27:12 +0600

    refactor(app/ZarinaApp.kt): "Dagger 2" replaced with "Hilt"

--- app/src/main/java/com/ren/zarinacapital/ZarinaApp.kt
+++ app/src/main/java/com/ren/zarinacapital/ZarinaApp.kt
@@ -1,25 +1,7 @@
 package com.ren.zarinacapital
 
 import android.app.Application
-import com.ren.di.dependencies.FeatureDependenciesProvider
-import com.ren.di.dependencies.HasFeatureDependencies
-import com.ren.forexapi.impl.di.DaggerNetworkComponent
-import com.ren.zarinacapital.di.AppComponent
-import com.ren.zarinacapital.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
-class ZarinaApp : Application(), HasFeatureDependencies {
-
-    override lateinit var featureDependencies: FeatureDependenciesProvider
-
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.builder()
-            .applicationContext(applicationContext)
-            .networkApi(DaggerNetworkComponent.factory().create(BuildConfig.FOREX_BASE_URL))
-            .build()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        featureDependencies = appComponent.featureDependencies
-    }
-}
+@HiltAndroidApp
+class ZarinaApp : Application()
