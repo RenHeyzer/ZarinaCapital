977f8469057aaaac9f68d6f3e6408561b3126015
RenHeyzer <vesolyyradin@gmail.com>
2024-04-29 22:17:14 +0600

    refactor(core:di): removed all files

--- core/di/src/main/java/com/ren/di/keys/FeatureDependenciesKey.kt
+++
@@ -1,9 +0,0 @@
-package com.ren.di.keys
-
-import com.ren.di.dependencies.FeatureDependencies
-import dagger.MapKey
-import kotlin.reflect.KClass
-
-@MapKey
-@Target(AnnotationTarget.FUNCTION)
-annotation class FeatureDependenciesKey(val value: KClass<out FeatureDependencies>)
