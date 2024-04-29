79f6a696fad7250dd40ec88abb914dcfe1acaa6b
RenHeyzer <vesolyyradin@gmail.com>
2024-04-29 22:35:23 +0600

    refactor(features:auth/AuthModule.kt): removed "Dagger 2" code from module and add "Hilt"
    * Removed Dagger 2 component - "AuthComponent"
    * Removed binds of "ViewModelFactory" and "SignUpViewModel"
    * Module installed in "ViewModelComponent" of "Hilt"
    * Added "@ViewModelScoped" to binds functions

--- features/auth/src/main/java/com/ren/auth/internal/di/AuthModule.kt
+++ features/auth/src/main/java/com/ren/auth/internal/di/AuthModule.kt
@@ -1,51 +1,32 @@
 package com.ren.auth.internal.di
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.ren.auth.api.dependencies.AuthDeps
 import com.ren.auth.api.domain.entities.User
 import com.ren.auth.api.domain.repositories.AuthRepository
 import com.ren.auth.internal.data.mappers.UserMapper
 import com.ren.auth.internal.data.repositories.AuthDataRepository
 import com.ren.auth.internal.domain.entities.SignUpParams
 import com.ren.auth.internal.domain.mappers.SignUpParamsMapper
-import com.ren.auth.internal.presentation.ui.viewmodels.SignUpViewModel
 import com.ren.common.Mapper
-import com.ren.di.keys.ViewModelKey
-import com.ren.di.scopes.FeatureScope
 import com.ren.forexapi.api.models.UserDTO
-import com.ren.presentation.base.ViewModelFactory
 import dagger.Binds
-import dagger.Component
 import dagger.Module
-import dagger.multibindings.IntoMap
-
-@[FeatureScope Component(
-    modules = [AuthModule::class],
-    dependencies = [AuthDeps::class]
-)]
-internal interface AuthComponent {
-
-    val viewModelFactory: ViewModelProvider.Factory
-}
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
+@InstallIn(ViewModelComponent::class)
 internal interface AuthModule {
 
     @Binds
+    @ViewModelScoped
     fun bindUserMapper(mapperImpl: UserMapper): Mapper<UserDTO, User>
 
     @Binds
+    @ViewModelScoped
     fun bindSignUpParamsMapper(mapperImpl: SignUpParamsMapper): Mapper<SignUpParams, User>
 
     @Binds
+    @ViewModelScoped
     fun bindAuthRepository(repositoryImpl: AuthDataRepository): AuthRepository
-
-    @Binds
-    fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(SignUpViewModel::class)
-    fun signUpViewModel(viewModel: SignUpViewModel): ViewModel
 }
