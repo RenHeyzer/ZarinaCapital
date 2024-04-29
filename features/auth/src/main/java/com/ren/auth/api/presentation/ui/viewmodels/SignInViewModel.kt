86e4fe9bd1789f32fd81c5785d481e94747d139b
RenHeyzer <vesolyyradin@gmail.com>
2024-04-29 22:43:36 +0600

    additionally(features.auth/SignInViewModel.kt): added "@HiltViewModel" annotation and "@Inject constructor()"

--- features/auth/src/main/java/com/ren/auth/api/presentation/ui/viewmodels/SignInViewModel.kt
+++ features/auth/src/main/java/com/ren/auth/api/presentation/ui/viewmodels/SignInViewModel.kt
@@ -1,6 +1,9 @@
 package com.ren.auth.api.presentation.ui.viewmodels
 
 import com.ren.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SignInViewModel : BaseViewModel() {
+@HiltViewModel
+class SignInViewModel @Inject constructor() : BaseViewModel() {
 }
