51bd6d3ec01417c9c9db62952c9530738cae9695
RenHeyzer <vesolyyradin@gmail.com>
2024-04-30 17:39:39 +0600

    refactor(features:auth/ConfirmEmail(Fragment * ViewModel)): moved from "api" package
    * "ConfirmEmailFragment.kt" and "ConfirmEmailViewModel.kt" moved from "api/presentation/ui/..." to "internal/presentation/ui/..."
    
    * Both files marked with "internal" visibility modifier

--- features/auth/src/main/java/com/ren/auth/internal/presentation/ui/viewmodels/ConfirmEmailViewModel.kt
+++ features/auth/src/main/java/com/ren/auth/internal/presentation/ui/viewmodels/ConfirmEmailViewModel.kt
@@ -1,9 +1,9 @@
-package com.ren.auth.api.presentation.ui.viewmodels
+package com.ren.auth.internal.presentation.ui.viewmodels
 
 import com.ren.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ConfirmEmailViewModel @Inject constructor() : BaseViewModel() {
+internal class ConfirmEmailViewModel @Inject constructor() : BaseViewModel() {
 }
