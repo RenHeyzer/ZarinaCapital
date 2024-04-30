9703204eb6724ddf666198ff42dc4c4e266b7422
RenHeyzer <vesolyyradin@gmail.com>
2024-04-30 16:30:15 +0600

    fix(forex-api/VerificationCodeDTO): changed "code" to "otp"
    * Changed @SerializedName("code") to "otp"
    * Changed property "code" to "otp"

--- forex-api/src/main/java/com/ren/forexapi/api/models/VerificationCodeDTO.kt
+++ forex-api/src/main/java/com/ren/forexapi/api/models/VerificationCodeDTO.kt
@@ -3,6 +3,6 @@ package com.ren.forexapi.api.models
 import com.google.gson.annotations.SerializedName
 
 data class VerificationCodeDTO(
-    @SerializedName("code")
-    val code: Int
+    @SerializedName("otp")
+    val otp: Int
 )
