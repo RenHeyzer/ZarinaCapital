60880cce25b948ad31cf82f3963c23e72d955b54
RenHeyzer <vesolyyradin@gmail.com>
2024-05-06 01:15:06 +0600

    additionally(features:auth/ConfirmEmailViewModel.kt): Processing email confirmation request
    * Processing email confirmation request from "ConfirmEmailUseCase.kt" to "_resultState" "LiveData" with "UIState".

--- features/auth/src/main/java/com/ren/auth/internal/presentation/ui/viewmodels/ConfirmEmailViewModel.kt
+++ features/auth/src/main/java/com/ren/auth/internal/presentation/ui/viewmodels/ConfirmEmailViewModel.kt
@@ -1,9 +1,34 @@
 package com.ren.auth.internal.presentation.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ren.auth.internal.domain.usecases.ConfirmEmailUseCase
 import com.ren.presentation.base.BaseViewModel
+import com.ren.presentation.utils.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-internal class ConfirmEmailViewModel @Inject constructor() : BaseViewModel() {
+internal class ConfirmEmailViewModel @Inject constructor(
+    private val confirmEmailUseCase: ConfirmEmailUseCase
+) : BaseViewModel() {
+
+    private val _resultState = MutableLiveData<UIState<Nothing>>()
+    val resultState: LiveData<UIState<Nothing>> = _resultState
+
+    fun confirmEmail(code: Int) {
+        viewModelScope.launch {
+            _resultState.value = UIState.Loading
+            confirmEmailUseCase(code).fold(
+                onSuccess = {
+                    _resultState.value = UIState.Success()
+                },
+                onFailure = {
+                    _resultState.value = UIState.Error(it)
+                }
+            )
+        }
+    }
 }
