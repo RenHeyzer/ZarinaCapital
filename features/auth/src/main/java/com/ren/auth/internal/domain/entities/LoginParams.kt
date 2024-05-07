54f88a2c200805fc0428eaebf8c8c5196486a178
RenHeyzer <vesolyyradin@gmail.com>
2024-05-08 02:41:15 +0600

    feature(features:auth): Login completed
    * Validate fields: email, password.
    * Navigate to "SignUpFragment.kt"
    * Get and save auth tokens to "TokenManager.kt"
    * Processing login request

--- features/auth/src/main/java/com/ren/auth/internal/domain/entities/LoginParams.kt
+++ features/auth/src/main/java/com/ren/auth/internal/domain/entities/LoginParams.kt
@@ -3,6 +3,6 @@ package com.ren.auth.internal.domain.entities
 import com.ren.common.Mappable
 
 data class LoginParams(
-    val email: String,
-    val password: String
+    val email: Pair<String, String?>,
+    val password: Pair<String, String?>
 ): Mappable
